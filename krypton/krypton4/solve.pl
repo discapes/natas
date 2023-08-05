10b2526b7890e4c4e9bbbda8ae52234df7c1c23a
Miika Tuominen <miika@beanbakers.fi>
2023-08-05 18:07:33 +0300

    krypton4

---
+++ krypton/krypton4/solve.pl
@@ -0,0 +1,61 @@
+#!/bin/perl
+use strict;
+use warnings;
+use v5.30;
+use autodie;
+use Data::Dump qw(dump);
+
+sub top5;
+sub rotn;
+
+my $sample = do {
+	local $/;<>
+};
+$sample =~ tr/ //ds;
+my @sample = split //, $sample;
+
+
+my @slots = ("") x 6;
+
+for (0 .. $#sample) {
+	$slots[$_ % 6] .= $sample[$_];
+}
+
+for (@slots) {
+	state $i;
+	say "Slot " . $i++;
+	for my $i (1 .. 26) {
+		tr/A-Z/B-ZA/;
+		my @top = top5 split //, $_;
+		say "ROT$i: " . join ", ", @top if "E" ~~ @top and "T" ~~ @top;
+	}
+}
+
+$slots[0] = rotn $slots[0], 21;
+$slots[1] = rotn $slots[1], 9;
+$slots[2] = rotn $slots[2], 22;
+$slots[3] = rotn $slots[3], 16;
+$slots[4] = rotn $slots[4], 22;
+$slots[5] = rotn $slots[5], 2;
+
+for (0 .. $#sample) {
+	print substr $slots[$_ % 6], int($_/6), 1;
+}
+
+sub rotn {
+	my ($str, $n) = @_;
+	for my $i (1 .. $n) {
+		$str =~ tr/A-Z/B-ZA/;
+	}
+	$str;
+}
+
+sub top5 {
+	my (@arr) = @_;
+	my %monograms;
+	for (0 .. $#arr) {
+		$monograms{$arr[$_]}++;
+	}
+	my @monograms = sort { $monograms{$b} <=> $monograms{$a} } keys %monograms;
+	@monograms[0..6];
+}
